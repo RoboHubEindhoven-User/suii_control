48ef6d7f34dbdee53eb3f44f942e59303a5d10b9
RoboHubEindhoven <robohubeindhoven@gmail.com>
2019-05-24 11:09:59 +0200

    updated suii_refbox_client

---
+++ suii_refbox_client/include/suii_refbox_client/refbox_config.h
@@ -0,0 +1,14 @@
+/************** Copyright (c) 2019, RoboHubEindhoven. All rights reserved **************
+ * @Project     : suii_refbox_client
+ * @file        : refbox_config.h
+ *
+ * @brief       : This file specifies the configurations to be used by the suii refbox 
+ *                client node.
+ *
+ * @author      : Thierry Zinkeng
+ * Contact:     : thi.zinkeng@gmail.com
+ *
+ ***************************************************************************************/
+#pragma once
+
+#define DEBUG_REFBOX 1
